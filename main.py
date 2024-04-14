6b59fbee250303f4542fae29579856900a781616
Anh Cao <anhcao4922@gmail.com>
2024-04-14 10:48:54 +0300

    implement the fetching data and store to text file
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

--- main.py
+++ main.py
@@ -4,83 +4,60 @@ import openpyxl
 
 def main(): 
 	# Website URL 
-	url = "https://www.tratencongty.com/"
+	url = "https://thongtincongty.org/tp-ho-chi-minh/"
+	total_pages = 100
 
-	# Send GET request and store content
+	for index_page in range(total_pages): 
+		html_content = do_get_request_and_return_response_content(url + '/page/' + str(index_page + 1))
+
+		# Parse HTML content
+		soup = BeautifulSoup(html_content, 'html.parser')
+
+		# Get Link data from HTML result
+		for result in soup.find_all('div', class_='col post-item'):
+			link = result.find('a')['href']
+
+			company_html_content = do_get_request_and_return_response_content(link)
+   
+			# Parse HTML content
+			company_soup = BeautifulSoup(company_html_content, 'html.parser')
+			company_info = {}
+
+			for div in company_soup.find_all('div', class_='ttdn'):
+				label = div.find('label').text.strip()
+				if div.find('span', class_='gia-tri'):
+					value = div.find('span', class_='gia-tri').text.strip()
+				elif div.find('a', class_='gia-tri'):
+					value = div.find('a', class_='gia-tri').text.strip()
+				company_info[label] = value
+
+			# Append dict data to text file
+			append_text(company_info)
+
+
+def do_get_request_and_return_response_content(url: str) -> bytes:
 	try:
 		response = requests.get(url)
 		response.raise_for_status()  # Raise an error for unsuccessful requests
-		html_content = response.content
+		return response.content
 	except requests.exceptions.RequestException as e:
 		print(f"Error: {e}")
 		exit()
-
-	# Parse HTML content
-	soup = BeautifulSoup(html_content, 'html.parser')
-
-	# Extract data from search results
-	data = []
-	for result in soup.find_all('div', class_='search-results'):
-		name = result.find('a').text.strip()
-		link = result.find('a')['href']
-		company_info = {'Name': name, 'Link': link}
-		contact_info = string_to_dict(result.find('p').text.split('-')[1].strip())
-
-		# Concat Append the data to the list
-		data.append({**company_info, **contact_info})
-
-	# Create a workbook
-	wb = openpyxl.Workbook()
-
-	# Get the active worksheet
-	ws = wb.active
-
-	# Set column headers
-	ws.cell(row=1, column=1).value = "Name"
-	ws.cell(row=1, column=2).value = "Link"
-	ws.cell(row=1, column=3).value = "Đại diện pháp luật"
-	ws.cell(row=1, column=4).value = "Địa chỉ"
-
-	# Iterate through the list and write data to cells
-	row_num = 2
-	for company_data in data:
-		ws.cell(row=row_num, column=1).value = company_data['Name']
-		ws.cell(row=row_num, column=2).value = company_data['Link']
-		ws.cell(row=row_num, column=3).value = company_data['Đại diện pháp luật']
-		ws.cell(row=row_num, column=4).value = company_data['Địa chỉ']
-		row_num += 1
-
-	# Save the workbook with a filename
-	filename = "company_data.xlsx"
-	wb.save(filename)
-
-	print(f"Company data exported to Excel file: {filename}")
-
-
-def string_to_dict(text: str):
+    
+def append_text(dict_data):
 	"""
-	Converts a string with key-value pairs on separate lines to a dictionary.
+	Appends text to a text file and have a new line right after.
 
 	Args:
-		text: The string containing key-value pairs.
-
-	Returns:
-		A dictionary with extracted key-value pairs.
+		text: The text to append to the file.
 	"""
-	# Split the string by lines
-	lines = text.splitlines()
-
-	# Initialize an empty dictionary
-	data = {}
-
-	# Loop through each line
-	for line in lines:
-		# Remove extra whitespace and split by the first colon
-		key, value = line.strip().split(":", 1)
-		# Add the key-value pair to the dictionary
-		data[key] = value
-
-	return data
+	filename = 'company_info.txt'
+	with open(filename, 'a') as file:
+		for _, value in dict_data.items():  
+			file.write('%s,' % (value))
+		file.write("\n")
+		file.close()
+    
 
 if __name__ == "__main__":
     main() 
